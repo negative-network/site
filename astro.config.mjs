5eb2948a408c2f8845e832e3c7d73fa24dc8d634
Pierre Blarre <pierre.blarre@gmail.com>
2024-07-21 13:27:02 +0200

    Trying to deploy for negative-network.com domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://negative-network.github.io/',
-  base: '/',
+  site: 'https://negative-network.com',
+  
+  // site: 'https://negative-network.github.io',
+  // base: '/site',
 
-  // site: 'https://negative-network.com',
   // site: 'https://nn-site.test',
   integrations: [tailwind(), sitemap()]
 });
