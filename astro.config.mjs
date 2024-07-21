85dd2a88bc057e9f0a94a503523855fa0735eced
Pierre Blarre <pierre.blarre@gmail.com>
2024-07-21 13:12:56 +0200

    again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://negative-network.com',
+  // site: 'https://negative-network.com',
+  site: 'https://negative-network.github.io/site/',
   // site: 'https://nn-site.test',
   integrations: [tailwind(), sitemap()]
 });
